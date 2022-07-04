5f9f1ea3b42222f139a7af5f2b22d0d111da5a21
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2022-07-03 21:13:39 -0300

    Update Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,8 +15,7 @@ public class Program {
 		Tabuleiro tb = new Tabuleiro(8, 8);
 		Partida partida = new Partida(tb);
 		Scanner sc = new Scanner(System.in);
-		Dama piece = new Dama(tb, new Posicao(0, 0) ,partida.getJogadorCorreto());
-		
+		Dama piece = new Dama(tb, new Posicao(0, 0), partida.getJogadorCorreto());
 
 		partida.preencherTabuleiro();
 		System.out.println("");
@@ -26,64 +25,63 @@ public class Program {
 		tb.imprimirTabuleiro();
 
 		while (tb.quantPieceBrancas() != 0 && tb.quantPiecePretas() != 0) {
-		  try {
-			System.out.println();
-			System.out.println("VEZ DO JOGADOR:  " + partida.getJogadorCorreto());
-			System.out.println();
-			System.out.print("Digite a posicao da peca q voce quer mover: ");
-			String posicaoPartida = sc.nextLine();
-			Posicao inicio = Posicao.converteVetor(posicaoPartida, tb);
-			
-			if(inicio.validarPosicaoInicio(partida,tb) == false) {
-				System.out.println("Essa posicao é invalida ");
-			}
-			
-			while (inicio == null || inicio.validarPosicaoInicio(partida,tb) == false) {
+			try {
 				System.out.println();
-				tb.imprimirTabuleiro();
-				System.out.print("Digite novamente posicao da peca q voce quer mover: ");
-				posicaoPartida = sc.nextLine();
-				inicio = Posicao.converteVetor(posicaoPartida, tb);
-				if(inicio.validarPosicaoInicio(partida,tb) == false) {
-					System.out.println("Essa posicao é invalida ");
-				}
-			}
-			System.out.print("Digite a posicao de destino: ");
-			String posicaoChegada = sc.nextLine();
-			Posicao chegada = Posicao.converteVetor(posicaoChegada, tb);
-			while (chegada == null) {
+				System.out.println("VEZ DO JOGADOR:  " + partida.getJogadorCorreto());
 				System.out.println();
-				tb.imprimirTabuleiro();
-				System.out.print("Digite novamente a posicao de destino: ");
-				System.out.println();
-				posicaoPartida = sc.nextLine();
-				chegada = Posicao.converteVetor(posicaoPartida,tb);
-			}
+				System.out.print("Digite a posicao da peca que voce quer mover: ");
+				String posicaoPartida = sc.nextLine();
+				Posicao inicio = Posicao.converteVetor(posicaoPartida, tb);
 
-			partida.jogar(inicio, chegada);
-			System.out.println();
-			piece.mudarCor();
-			tb.imprimirTabuleiro();
-			if(piece.existeMovimentosAinda(piece.getCor())== false) {
-				break;
+				if (inicio.validarPosicaoInicio(partida, tb) == false) {
+					System.out.println("Essa posicao ï¿½ invalida ");
+				}
+
+				while (inicio == null || inicio.validarPosicaoInicio(partida, tb) == false) {
+					System.out.println();
+					tb.imprimirTabuleiro();
+					System.out.print("Digite novamente posicao da peca que voce quer mover: ");
+					posicaoPartida = sc.nextLine();
+					inicio = Posicao.converteVetor(posicaoPartida, tb);
+					if (inicio.validarPosicaoInicio(partida, tb) == false) {
+						System.out.println("Essa posicao ï¿½ invalida ");
+					}
+				}
+				System.out.print("Digite a posicao de destino: ");
+				String posicaoChegada = sc.nextLine();
+				Posicao chegada = Posicao.converteVetor(posicaoChegada, tb);
+				while (chegada == null) {
+					System.out.println();
+					tb.imprimirTabuleiro();
+					System.out.print("Digite novamente a posicao de destino: ");
+					System.out.println();
+					posicaoPartida = sc.nextLine();
+					chegada = Posicao.converteVetor(posicaoPartida, tb);
+				}
+
+				partida.jogar(inicio, chegada);
+				System.out.println();
+				piece.mudarCor();
+				tb.imprimirTabuleiro();
+				if (piece.existeMovimentosAinda(piece.getCor()) == false) {
+					break;
+				}
+			} catch (RuntimeException e) {
+				e.getMessage();
 			}
-		  }
-		  catch(RuntimeException e) {
-			  e.getMessage();
-		  }
 		}
 		sc.close();
-		if(piece.existeMovimentosAinda(piece.getCor())== false) {
+		if (piece.existeMovimentosAinda(piece.getCor()) == false) {
 			piece.mudarCor();
 		}
-		if(tb.quantPieceBrancas() == 0) {
+		if (tb.quantPieceBrancas() == 0) {
 			piece.setCor(Cor.PRETO);
 		}
-		if(tb.quantPiecePretas() == 0) {
+		if (tb.quantPiecePretas() == 0) {
 			piece.setCor(Cor.BRANCO);
 		}
-		
-		System.out.println("PARABÉNS JOGADOR "+piece.getCor() + "VOCE VENCEU!!!");
+
+		System.out.println("PARABï¿½NS JOGADOR " + piece.getCor() + "VOCE VENCEU!!!");
 
 	}
 }
